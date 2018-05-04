aa248b50959da4baab93eb11467f6513865034e5
Diego Meza <diego_giova@hotmail.com>
2018-05-04 17:54:38 -0500

    Contact section innit

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -6,11 +6,80 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SiteController extends Controller
 {
+    /**
+     * Render and return a Response with the contact page.
+     *
+     * @param
+     * @return Symfony\Component\HttpFoundation\Response;
+     */
+    public function contact()
+    {
+        return $this->render('site/pages/contact.html.twig');
+    }
+
+    /**
+     * Render and return a Response with the index page.
+     *
+     * @param
+     * @return Symfony\Component\HttpFoundation\Response;
+     */
     public function index()
     {
-        $number = mt_rand(0, 100);
-        return $this->render('site/pages/index.html.twig', [
-            'number' => $number,
-        ]);
+        return $this->render('site/pages/index.html.twig');
+    }
+
+    /**
+     * Render and return a Response with the privacy policy page.
+     *
+     * @param
+     * @return Symfony\Component\HttpFoundation\Response;
+     */
+    public function privacyPolicy()
+    {
+        return $this->render('site/pages/privacy-policy.html.twig');
+    }
+
+    /**
+     * Render and return a Response with the blog page.
+     *
+     * @param
+     * @return Symfony\Component\HttpFoundation\Response;
+     */
+    public function blog()
+    {
+        return $this->render('site/pages/blog.html.twig');
+    }
+
+    /**
+     * Render and return a Response with the search page.
+     *
+     * @param
+     * @return Symfony\Component\HttpFoundation\Response;
+     */
+    public function search()
+    {
+        return $this->render('site/pages/search.html.twig');
+    }
+
+    /**
+     * Render and return a Response with the shipping policy page.
+     *
+     * @param
+     * @return Symfony\Component\HttpFoundation\Response;
+     */
+    public function shippingPolicy()
+    {
+        return $this->render('site/pages/shipping-policy.html.twig');
+    }
+
+    /**
+     * Render and return a Response with the product detail page.
+     *
+     * @param
+     * @return Symfony\Component\HttpFoundation\Response;
+     */
+    public function productDetail()
+    {
+        return $this->render('site/pages/product-detail.html.twig');
     }
 }
